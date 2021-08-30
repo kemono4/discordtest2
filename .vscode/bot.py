4a42dfbc2f44428f56a246334c79301cbaf6081b
kemono4 <67272864+kemono4@users.noreply.github.com>
2021-08-30 16:26:30 +0800

    3

--- .vscode/bot.py
+++ .vscode/bot.py
@@ -2,28 +2,33 @@ import discord
 from discord.ext import commands
 import json
 import random
+<<<<<<< HEAD:.vscode/bot.py
 import os 
+=======
+>>>>>>> parent of ff95a7c (Revert "randompic"):.vscode/main.py
 
-with open('setting.json',mode='r',encoding='utf8') as jfile:
+with open('s.json',mode='r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix= '?')
 
 @bot.event
 async def on_ready():
-    print(">> Bot is online <<")
+    channel = bot.get_channel(865888651597447178)
+    await channel.send('>> Bot is online <<')
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(881549015076974606)
+    channel = bot.get_channel(865888651597447178)
     await channel.send(f'{member}Join!')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(881549015076974606)
+    channel = bot.get_channel(865888651597447178)
     await channel.send(f'{member}Leave!')
 
 
+<<<<<<< HEAD:.vscode/bot.py
 
 
 for filename in os.listdir('./cmds'):
@@ -34,3 +39,12 @@ for filename in os.listdir('./cmds'):
 
 if __name__ == "__main__":
     bot.run(jdata['token'])
+=======
+@bot.command()
+async def pic(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file = pic)
+    
+bot.run(jdata['token'])
+>>>>>>> parent of ff95a7c (Revert "randompic"):.vscode/main.py
