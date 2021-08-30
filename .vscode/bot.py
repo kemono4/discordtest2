6074f8cdeacebffe323c602b742982f9304515cd
kemono4 <67272864+kemono4@users.noreply.github.com>
2021-08-30 16:25:25 +0800

    2

--- .vscode/bot.py
+++ .vscode/bot.py
@@ -1,11 +1,13 @@
 import discord
 from discord.ext import commands
 import json
+import random
+import os 
 
 with open('setting.json',mode='r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
-bot = commands.Bot(command_prefix= '{')
+bot = commands.Bot(command_prefix= '?')
 
 @bot.event
 async def on_ready():
@@ -21,13 +23,14 @@ async def on_member_remove(member):
     channel = bot.get_channel(881549015076974606)
     await channel.send(f'{member}Leave!')
 
-@bot.command()
-async def ping (ctx):
-    await ctx.send(f'{round(bot.latency*1000)} ms')
 
-@bot.command()
-async def pic(ctx):
-    pic = discord.File('/Users/kmno4/Documents/GitHub/discordtest2/.vscode/pic/中一中LOGO.jpg')
-    await ctx.send(file = pic)
 
-bot.run(jdata['token'])
+
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(F'cmds.{filename[:-3]}')
+
+
+
+if __name__ == "__main__":
+    bot.run(jdata['token'])
