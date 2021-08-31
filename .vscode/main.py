2981ddece1049c676e4818064a32254c40922289
kemono4 <67272864+kemono4@users.noreply.github.com>
2021-08-31 12:02:29 +0800

    onmessage6

--- .vscode/main.py
+++ .vscode/main.py
@@ -18,7 +18,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(881757019906932766)
+    channel = bot.get_channel(860024195613065256)
     await channel.send(f'{member.mention}歡迎來到中一中音遊社伺服器！')
 
 @bot.event
@@ -47,7 +47,7 @@ async def pic(ctx):
 bot.remove_command('help')
 
 @bot.command()
-async def help(ctx):
+async def rules(ctx):
     embed = discord.Embed(title="台中一中音遊社規", description="", color=0xeee657, timestamp=datetime.datetime.now())
 
     embed.add_field(name="社規第一條", value="無論何時何地，社長是何人，該屆社長皆為最強，實力不容質疑。", inline=False)
@@ -55,6 +55,7 @@ async def help(ctx):
     embed.add_field(name="社規第三條", value="凡是社長送出任何疑似下跪或“電神”等言論，必須以紅怒之表情符號提醒不可嘲諷，以維持社團良好風氣。", inline=False)
     embed.add_field(name="社規第四條", value="社長發出成績圖時社員應當送出跪兔，乃維持社團內社長與社員的位階差異，維持社團運作。", inline=False)
     embed.add_field(name="社規第五條：", value="若社長要求觸及時請務必執行，此舉乃維持社長之自信心，穩固音遊社的中心支柱，使音遊社能繼續發揚光大。\n\n這些都是中一中音遊社員必遵守的規定", inline=False)
+    await ctx.send(embed = embed)
 
 
 
